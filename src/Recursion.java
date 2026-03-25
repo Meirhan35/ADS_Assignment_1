8fd229882df5b8248592c263eab37666ef9703f5
Meirhan35 <meirhanabilda@gmail.com>
2026-03-25 06:32:59 +0500

    end

--- src/Recursion.java
+++ src/Recursion.java
@@ -1,3 +1,14 @@
+int f(int n,int a, int b){
+    if(n > 0){
+        int cur = a;
+        a = b;
+        b += cur;
+        return f(n - 1, a, b);
+    }
+    return b;
+}
 void main() {
-  $END$
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    System.out.println(f(n - 1, 0, 1));
 }
